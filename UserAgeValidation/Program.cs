500a040f9a424a024361f6f1da4100d3b6c0963f
Mattias Kitvel <mkitvel@tktk.ee>
2023-02-11 16:28:58 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,17 @@
+﻿//rakendus küsib kasutajal valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtude kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi]!"/ Welcome, Ms. [kasutaja perekonnanimi]!
+
+Console.WriteLine("Please, select your gender (m/f): ");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne formaadis)
+Console.WriteLine("Please enter your lastname:");
+string userLastname = Console.ReadLine();   
+
+
+if (userGender == 'm')
+{ Console.WriteLine($"Welcome, Mr. {userLastname} !"); }
+else if (userGender == 'f')
+{ Console.WriteLine($"Welcome, Ms. {userLastname} !"); }
+else { Console.WriteLine("Welcome"); }
---
+++ UserAgeValidation2/Program.cs
@@ -0,0 +1,13 @@
+﻿//rakendus küsib kasutajal sisestada tema vanuse
+//kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse:
+// "You´re too young to use Instagram
+//muul juhul
+//konsoolis kuvatakse "Welcome to Instagram!
+Console.WriteLine("Enter your age:");
+
+int userAge = Int32.Parse(Console.ReadLine()); //"13" - heap, 13 - stack
+
+if(userAge >= 13)
+{ Console.WriteLine("Welcome to Instagram!"); }
+else
+{ Console.WriteLine("You´re too young to use Instagram!"); }
